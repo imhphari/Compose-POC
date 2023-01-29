41cd886a559e7c02cf5276acd619916484219b51
Hari Prasath <imhphari@gmail.com>
2023-01-29 11:19:48 +0530

    Restructured package name and modules

---
+++ app/src/main/java/com/android/base_app/Application.kt
@@ -0,0 +1,11 @@
+package com.android.base_app
+
+import android.app.Application
+
+/**
+ * Application class - Initialize core components for th app on startup.
+ */
+class Application: Application() {
+    
+    // Todo: Initialize dependencies.
+}
